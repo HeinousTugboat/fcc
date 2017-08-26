341f5a38fded8c13372e99c32e62c5a8cbfe43bf
HeinousTugboat <admin@heinousindustries.com>
2017-08-26 01:19:05 -0400

    feat(calculator): refactors, adds execute routine

--- calculator/calculator.ts
+++ calculator/calculator.ts
@@ -102,140 +102,137 @@ function isOperand(op: Token): op is Operand {
 
 /** The core class. This should be the central class for processing the math. */
 class Calculator {
-    // private output: Token[] = [];
-    // private input: Token[] = [];
-    // private operators: Operator[] = [];
-
+    private exprTokens: Token[];
+    private rpnTokens: Token[];
+    private history: string[] = [];
     constructor() {
         Operator.register(Add);
         Operator.register(Subtract);
         Operator.register(Multiply);
         Operator.register(Divide);
     }
-
-    // setOperation(op: Operator) {
-    // }
-    // addNumber(num: number) {
-    // }
-
-    // add(num: number) { }
-    // subtract(num: number) { }
-    // divide(num: number) { }
-    // multiply(num: number) { }
-    // clear() { }
-    // allClear() { }
-}
-
-/**
- * Converts an array of symbols into an array of Tokens. Checks if the symbol is
- * registered in the Operator dictionary first, and if it isn't, casts the
- * symbol to a number.
- */
-function convert(arr: string[]): Token[] {
-    return arr.reduce((acc: Token[], val: string) => {
-        if (Operator.dict[val]) {
-            acc.push(new Operator.dict[val]);
-        } else if (val === '(' || val === ')') {
-            acc.push(val);
-        } else {
-            acc.push(+val);
-        }
-        return acc;
-    }, <Token[]>[]);
-}
-
-/**
- * Converts an array of Tokens back into a string, either by referencing the
- * Operator's symbol or by casting the Opernad to a string.
- */
-function parseToString(arr: Token[]): string {
-    return arr.reduce((acc, val) => {
-        if (isOperator(val)) {
-            acc += val.symbol;
-        } else {
-            acc += '' + val;
-        }
-        return acc + ' ';
-    }, '');
-}
-
-/**
- * Dijkstra's [Shunting-yard
- * Algorithm](https://www.wikiwand.com/en/Shunting-yard_algorithm). Sort of.
- * Doesn't handle Precedence or Associativity yet.
- */
-function shuntingYard(tokens: Token[]) {
-    let input: Token[] = [...tokens];
-    let output: Token[] = [];
-    let operators: (Operator|'('|')')[] = [];
-    while (input.length) {
-        let token = input.shift();
-        if (!token) { continue; }
-        if (isOperand(token)) {
-            output.push(token)
-        } else if (isOperator(token)) {
-            let op = operators.pop();
-            while (op && isOperator(op) && op.precedence >= token.precedence && op.associativity === 'Left') {
-                output.push(op);
-                op = operators.pop();
+    /**
+     * Converts an array of symbols into an array of Tokens. Checks if the symbol is
+     * registered in the Operator dictionary first, and if it isn't, casts the
+     * symbol to a number.
+     */
+    private convert(str: string): Token[] {
+        return str.split('').reduce((acc: Token[], val: string) => {
+            if (Operator.dict[val]) {
+                acc.push(new Operator.dict[val]);
+            } else if (val === '(' || val === ')') {
+                acc.push(val);
+            } else {
+                acc.push(+val);
             }
-            op && operators.push(op);
-            operators.push(token);
-        } else if (token == '(') {
-            operators.push(token);
-        } else if (token == ')') {
-            let op = operators.pop();
-            while (op && op !== '(') {
-                isOperator(op) && output.push(op);
-                op = operators.pop();
-                if (!op) {
-                    throw new Error('Did not find a left bracket!');
+            return acc;
+        }, <Token[]>[]);
+    }
+
+    /**
+     * Converts an array of Tokens back into a string, either by referencing the
+     * Operator's symbol or by casting the Opernad to a string.
+     */
+    private parseToString(arr: Token[]): string {
+        return arr.reduce((acc, val) => {
+            if (isOperator(val)) {
+                acc += val.symbol;
+            } else {
+                acc += '' + val;
+            }
+            return acc + '';
+        }, '');
+    }
+
+    /**
+     * Dijkstra's [Shunting-yard
+     * Algorithm](https://www.wikiwand.com/en/Shunting-yard_algorithm). Sort of.
+     * Doesn't handle Precedence or Associativity yet.
+     */
+    private shuntingYard(tokens: Token[]) {
+        let input: Token[] = [...tokens];
+        let output: Token[] = [];
+        let operators: (Operator|'('|')')[] = [];
+        while (input.length) {
+            let token = input.shift();
+            if (!token) { continue; }
+            if (isOperand(token)) {
+                output.push(token)
+            } else if (isOperator(token)) {
+                let op = operators.pop();
+                while (op && isOperator(op) && op.precedence >= token.precedence && op.associativity === 'Left') {
+                    output.push(op);
+                    op = operators.pop();
+                }
+                op && operators.push(op);
+                operators.push(token);
+            } else if (token == '(') {
+                operators.push(token);
+            } else if (token == ')') {
+                let op = operators.pop();
+                while (op && op !== '(') {
+                    isOperator(op) && output.push(op);
+                    op = operators.pop();
+                    if (!op) {
+                        throw new Error('Did not find a left bracket!');
+                    }
                 }
             }
         }
+        while (operators.length) {
+            let op = operators.pop();
+            op && output.push(op);
+        }
+        return {
+            output, input, operators
+        };
     }
-    while (operators.length) {
-        let op = operators.pop();
-        op && output.push(op);
+
+    execute(): number {
+        let input = [...this.rpnTokens];
+        let stack: Token[] = [];
+        while (input.length) {
+            let op = input.shift();
+            if (!op) break;
+            if (isOperand(op)) {
+                stack.push(op);
+            } else if (isOperator(op)) {
+                let b = stack.pop();
+                let a = stack.pop();
+                stack.push(op.execute(<Operand>a,<Operand>b));
+            }
+        }
+        if (stack.length > 1) {
+            throw new Error('Items left in the stack! Ohnoes! :>> '+JSON.stringify(stack));
+        }
+        return stack[0] as Operand;
     }
-    return {
-        output, input, operators
-    };
+
+    set expression(str: string) {
+        this.history.push(str);
+        this.exprTokens = this.convert(str);
+        this.rpnTokens = this.shuntingYard(this.exprTokens).output;
+    }
+    get expression(): string {
+        return this.parseToString(this.exprTokens);
+    }
 }
 
-// while there are tokens to be read:
-// 	read a token.
-// 	if the token is a number, then push it to the output queue.
-// 	if the token is an operator, then:
-// 		while there is an operator at the top of the operator stack with
-// 			greater than or equal to precedence and the operator is left associative:
-// 				pop operators from the operator stack, onto the output queue.
-// 		push the read operator onto the operator stack.
-// 	if the token is a left bracket (i.e. "("), then:
-// 		push it onto the operator stack.
-// 	if the token is a right bracket (i.e. ")"), then:
-// 		while the operator at the top of the operator stack is not a left bracket:
-// 			pop operators from the operator stack onto the output queue.
-// 		pop the left bracket from the stack.
-// 		/* if the stack runs out without finding a left bracket, then there are
-// 		mismatched parentheses. */
-// if there are no more tokens to read:
-//     while there are still operator tokens on the stack:
-//         /* if the operator token on the top of the stack is a bracket, then
-//         there are mismatched parentheses. */
-//         pop the operator onto the output queue.
-// exit.
+
 
 
 /******************************************************************************/
 // Execution Area:
 
 let calc = new Calculator;
-let testString = '2*(2+2+2)*2';
-
-let results = shuntingYard(convert(testString.split('')));
-console.log(results);
-console.log(parseToString(results.output));
-// console.log(parseToString(convert(testString.split(''))));
-console.log(eval(testString));
+calc.expression = '2*(2+2+2)*2';
+console.log(calc.execute());
+calc.expression = '4+(9/3)*(2*4/2)';
+console.log(calc.execute());
 // 1 2 4 / + 3 - 4 5 6 + * +
+calc.expression = '2';
+console.log(calc.execute());
+calc.expression += '+3';
+console.log(calc.execute());
+calc.expression += '*4';
+console.log(calc.execute());
