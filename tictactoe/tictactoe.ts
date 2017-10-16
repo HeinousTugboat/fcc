71d8e13596d1191d62e178e51993b9e8aaf1e26b
HeinousTugboat <admin@heinousindustries.com>
2017-10-15 23:42:42 -0400

    feat(tic-tac): added board and basic animations and clickhandler

--- tictactoe/tictactoe.ts
+++ tictactoe/tictactoe.ts
@@ -5,3 +5,25 @@
 // User Story: I can play a game of Tic Tac Toe with the computer.
 // User Story: My game will reset as soon as it's over so I can play again.
 // User Story: I can choose whether I want to play as X or O.
+
+
+class TicTacToe {
+    private board: HTMLElement;
+    constructor() {
+        this.board = document.getElementById('board') as HTMLElement;
+        this.board.addEventListener('click', this.click, true);
+    }
+    click(ev: Event) {
+        const el: HTMLElement = ev.target as HTMLElement;
+        let loc = el.dataset.loc;
+        if (!loc || el.dataset.mark !== undefined) {
+            return;
+        }
+        (el.querySelector('svg') as SVGElement).innerHTML = '<use href="#X"/>';
+        el.dataset.mark = 'X';
+        console.log(el);
+    }
+    reset() {}
+}
+
+const game = new TicTacToe;
